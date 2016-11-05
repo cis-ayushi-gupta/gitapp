9b19aeb2560330f056b4088ee608c1a4697e52c0
cis-ayushi-gupta <ayushi.g@cisinlabs.com>
2016-11-05 16:47:50 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gitapp::Application.config.secret_token = '2601b5b1a1963c954f0b4b3f932e0f1921fb4c7cfb91e3707a8b12696db03cd7a5e80de4d62a6320a621be71c64684d1814b4b0135bf1f94a5349398308ff038'
